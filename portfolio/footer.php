530b008156070044970017edde1b87ef11ffad6a
Hedlundaren <hedlundaren93@hotmail.com>
2015-09-23 17:03:24 +0200

    Portfolio added

---
+++ portfolio/footer.php
@@ -0,0 +1,10 @@
+<?php print ("<!DOCTYPE html>");?>
+
+<footer>
+
+<p class="ans" style="width:400px">Simon Hedlund</p>
+<button id="get_up" type="button" class="center" onClick = "up()"; >top</button>
+
+
+</body>
+</footer>
